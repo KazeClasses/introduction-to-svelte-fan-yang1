9cbb4b36907c0296b29e65df2c22d8c2a8ac9bba
fan yang <2104597407@qq.com>
2024-10-31 20:23:23 -0400

    fro

---
+++ code/src/components/Hello.svelte
@@ -0,0 +1,6 @@
+<script>
+    let name = 'world';
+</script>
+
+<input bind:value={name} placeholder="Enter your name" />
+<h1 class="text-4xl text-center text-blue-500">Hello {name}!</h1>
---
+++ code/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Navbar from '../components/Navbar.svelte';
+    import Hello from '../components/Hello.svelte';
+    let count = 0;
+
+    function handleClick() {
+        count += 1;
+    }
+
+    function isAnnoying() {
+        return count > 10;
+    }
+    
+</script>
+
+<Hello />
+
+<button on:click={handleClick}>
+    Poke me! {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+{#if isAnnoying()}
+    <p style="color:red;">You are annoying</p>
+{/if}
+
